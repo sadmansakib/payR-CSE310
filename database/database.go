c65aee7eb767e7c90a5faab5babb509e933580c8
zireael9797 <ryanhossain9797@gmail.com>
2020-09-09 15:47:19 +0600

    Restructure everything

--- database/database.go
+++ database/database.go
@@ -1,35 +1,58 @@
 package database
 
-type CustomerMock struct {
-	id       int    `json:"id,omitempty"`
-	fName    string `json:"first_name"`
-	lName    string `json:"last_name"`
-	email    string `json:"email"`
-	mobile   string `json:"mobile"`
-	password string `json:"password"`
+import "payR/models"
+
+var customer1 = models.Customer{
+	ID:       1,
+	FName:    "abc",
+	LName:    "edf",
+	Email:    "abc@example.com",
+	Password: "asd",
+	Mobile:   "0123456",
 }
 
-type Customer struct {
-	fName    string `json:"first_name"`
-	lName    string `json:"last_name"`
-	email    string `json:"email"`
-	mobile   string `json:"mobile"`
-	password string `json:"password"`
+var customer2 = models.Customer{
+	ID:       2,
+	FName:    "edf",
+	LName:    "ghi",
+	Email:    "ksk@example.com",
+	Password: "asd",
+	Mobile:   "0123458",
 }
 
-type Bill struct {
-	id             int     `json:"bill_id"`
-	customerID     int     `json:"id,omitempty"`
-	mobile         string  `json:"mobile"`
-	billType       string  `json:"bill_type"`
-	equipmentCount int     `json:"equipment_count"`
-	amount         float64 `json:"amount"`
-	accountID      int     `json:"account_id"`
-	paymentMethod  string  `json:"payment_method"`
-	submitDate     string  `json:"submition_data"`
+var billCustomer1 = models.Bill{
+	ID:             3,
+	CustomerID:     1,
+	Mobile:         "0123456",
+	BillType:       "Postpaid",
+	EquipmentCount: 1,
+	Amount:         550.00,
+	AccountID:      1,
+	PaymentMethod:  "credit Card",
+	SubmitDate:     "02/10/2020",
 }
 
-type Account struct {
-	id          int     `json:"account_id"`
-	totalAmount float64 `json:"total_amount"`
+var billCustomer2 = models.Bill{
+	ID:             25,
+	CustomerID:     2,
+	Mobile:         "0123458",
+	BillType:       "Postpaid",
+	EquipmentCount: 1,
+	Amount:         550.00,
+	AccountID:      1,
+	PaymentMethod:  "bkash",
+	SubmitDate:     "02/10/2020",
+}
+
+var accountState = models.Account{
+	ID:          1,
+	TotalAmount: 10500.00,
+}
+
+var Allcustomers = []models.Customer{
+	customer1, customer2,
+}
+
+var AllBills = []models.Bill{
+	billCustomer1, billCustomer2,
 }
